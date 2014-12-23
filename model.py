3d535d829fb8a706d5a8e69875f0490d798a4eec
Cyprian Guerra <cyprian.guerra@gmail.com>
2014-12-23 04:06:22 +0100

    getter for colour of the block

--- model.py
+++ model.py
@@ -10,3 +10,9 @@ def randomize():
     for row in range(len(data)):
         for col in range(len(data[row])):
             data[row][col] = random.randint(0, NUM_COLOURS-1)
+
+
+def get_block_colour(row, col):
+    if row >= 0 and row < len(data) and col >= 0 and col < len(data[row]):
+        return data[row][col]
+    return None
--- tests.py
+++ tests.py
@@ -19,4 +19,10 @@ for row in model.data:
 
 verify(num_colours == 5, 'Wrong amount of colours')
 
+model.data = [[1, 2], [3, 4]]
+verify(model.get_block_colour(0, 0) == 1, "Colour 0,0 not 1")
+verify(model.get_block_colour(0, 1) == 2, "Colour 0,1 not 2")
+verify(model.get_block_colour(1, 0) == 3, "Colour 1,0 not 3")
+verify(model.get_block_colour(1, 1) == 4, "Colour 1,1 not 4")
+
 print('END')
