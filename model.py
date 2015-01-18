877a28242fc112710280d3302cbe4cdca3ddb567
Cyprian Guerra <cyprian.guerra@gmail.com>
2015-01-18 01:11:59 +0100

    grid visualisation + fixes
    - FIX: data initialisation, grid size abstracted to constant
    - FIX: model.get_height() and model.get_width() return correct value
    - FIX: correct windowSurface size
    - ADD: grid display
    - model.randomize() -> model.init()
    - named imports instead of *

--- model.py
+++ model.py
@@ -1,13 +1,16 @@
 import random
 
-data = [[None] * 100] * 100
+data = []
 NUM_COLOURS = 5
+GRID_SIZE = 30
 
 
-def randomize():
-    for row in range(len(data)):
-        for col in range(len(data[row])):
-            data[row][col] = random.randint(0, NUM_COLOURS-1)
+def init():
+    for row in range(GRID_SIZE):
+        r = []
+        for col in range(GRID_SIZE):
+            r.append(random.randint(0, NUM_COLOURS - 1))
+        data.append(r)
 
 
 def get_block_colour(row, col):
@@ -17,8 +20,8 @@ def get_block_colour(row, col):
 
 
 def get_height():
-    return len(data)
+    return GRID_SIZE
 
 
 def get_width():
-    return len(data[0])
+    return GRID_SIZE
--- view.py
+++ view.py
@@ -1,6 +1,6 @@
 import pygame
 import sys
-from pygame.locals import *
+from pygame.locals import QUIT
 import model
 
 
@@ -19,12 +19,27 @@ PIECE_WIDTH = 30
 PIECE_HEIGHT = 30
 PIECE_MARGIN = 2
 
-
-windowSurface = pygame.display.set_mode((300, 300))
+windowSurface = pygame.display.set_mode((
+    model.get_width() * (PIECE_WIDTH + PIECE_MARGIN),
+    model.get_height() * (PIECE_HEIGHT + PIECE_MARGIN)
+))
 pygame.display.set_caption('Same Game')
+
+model.init()
 
 while True:
     windowSurface.fill(BLACK)
+    for row in range(model.get_height()):
+        for col in range(model.get_width()):
+            colour = model.get_block_colour(row, col)
+            if colour is not None:
+                pieceColour = PIECE_COLOURS[colour]
+                pygame.draw.rect(windowSurface, pieceColour, (
+                    col * (PIECE_WIDTH + PIECE_MARGIN),
+                    row * (PIECE_HEIGHT + PIECE_MARGIN),
+                    PIECE_WIDTH,
+                    PIECE_HEIGHT
+                ))
     pygame.display.update()
 
     for event in pygame.event.get():
